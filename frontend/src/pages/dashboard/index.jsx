d5d58b57846c1cf4a6cdee796848f098b5074397
Hemant Yadav <hemantsy66@gmail.com>
2026-09-13 13:50:21 +0530

    dashboard changes again

--- frontend/src/pages/dashboard/index.jsx
+++ frontend/src/pages/dashboard/index.jsx
@@ -8,7 +8,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import styles from "./index.module.css";
 import { BASE_URL } from '@/config';
 import { resetPostId } from '@/config/redux/reducer/postReducer';
-export default function dashBoard() {
+export default function dashboard() {
   const router = useRouter();
 
   const dispatch = useDispatch();
